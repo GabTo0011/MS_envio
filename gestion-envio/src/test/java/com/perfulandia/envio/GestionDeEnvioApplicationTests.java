5710b1812d246d06c5f1b342fda9935db36b073c
Gabriel T <gab.toledo@duocuc.cl>
2025-06-10 20:34:36 -0400

    Inicializa proyecto - add file README.md - add file .gitignore

---
+++ gestion-envio/src/test/java/com/perfulandia/envio/GestionDeEnvioApplicationTests.java
@@ -0,0 +1,13 @@
+package com.perfulandia.envio;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GestionDeEnvioApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
